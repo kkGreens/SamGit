682fce9c12180122636985780df2f51e45b74651
kk <karkuzhaliomrbranch@gmail.com>
2025-03-24 10:47:05 +0530

    second commit

---
+++ src/org/com/Second.java
@@ -0,0 +1,15 @@
+package org.com;
+
+public class Second {
+	
+	public void m1() {
+		System.out.println("Welcome");
+	}
+	
+	public static void main(String[] args) {
+		
+		Second s = new Second();
+		s.m1();
+	}
+
+}
