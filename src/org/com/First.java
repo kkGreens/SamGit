315a86c20e39511fafeaff7969102659f3904659
kk <karkuzhaliomrbranch@gmail.com>
2025-03-24 10:35:23 +0530

    intial commit

---
+++ src/org/com/First.java
@@ -0,0 +1,16 @@
+package org.com;
+
+public class First {
+	
+	public void m1() {
+		System.out.println("Apple");
+	}
+	
+	public static void main(String[] args) {
+		
+		First f = new First();
+		f.m1();
+		
+	}
+
+}
